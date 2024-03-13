027ec9427382f3c496e50d398249c03de5bf5625
OlikQueen <olysoz@mail.ru>
2024-03-13 22:04:48 +0300

    burgerMenu

--- counter.js
+++ counter.js
@@ -1,9 +1,8 @@
-// export function setupCounter(element) {
-//   let counter = 0
-//   const setCounter = (count) => {
-//     counter = count
-//     element.innerHTML = `count is ${counter}`
-//   }
-//   element.addEventListener('click', () => setCounter(counter + 1))
-//   setCounter(0)
-// }
+export function setupCounter(element) {
+    const btn = document.getElementById("crs");
+    const burgerMenu = document.querySelector(".burgerMenu");
+
+    btn.addEventListener('click', function() {
+        burgerMenu.setAttribute("style", "display: none;");
+        });
+}
