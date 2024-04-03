d3f3403b1a60eb58d4f71d1b24748436334077e5
OlikQueen <olysoz@mail.ru>
2024-04-03 10:58:34 +0300

    choice-city

--- src/js/choice-city.js
+++ src/js/choice-city.js
@@ -1,24 +1,42 @@
-export function choiceCity(){
+export function choiceCity() {
     // { name: "Москва", "subdomain": "msk"}
-    
+
     const citys = [
-        { name: "Москва", "subdomain": "msk"},
-        { name: "Санкт-Петербург", "subdomain": "spb"},
-        { name: "Калуга", "subdomain": "kaluga"},
-        { name: "Новосибирск", "subdomain": "novosibirsk"},
-        { name: "Владикавказ", "subdomain": "vladikavkaz"},
-        { name: "Казань", "subdomain": "kazan"},
+        {
+            name: "Москва",
+            "subdomain": "msk"
+        },
+        {
+            name: "Санкт-Петербург",
+            "subdomain": "spb"
+        },
+        {
+            name: "Калуга",
+            "subdomain": "kaluga"
+        },
+        {
+            name: "Новосибирск",
+            "subdomain": "novosibirsk"
+        },
+        {
+            name: "Владикавказ",
+            "subdomain": "vladikavkaz"
+        },
+        {
+            name: "Казань",
+            "subdomain": "kazan"
+        },
     ];
-    
+
     const city = document.querySelector("[data-choice-city]");
     const cityList = city.querySelector("[data-choice-list]");
 
     for (const iterator of citys) {
         const link = document.createElement('a');
-        link.innerHTML = iterator;
+        link.innerHTML = iterator.name;
         link.classList.add('choice-city__link');
 
-        const linkLi = document.createElement('li'); 
+        const linkLi = document.createElement('li');
         linkLi.classList.add('choice-city__item');
 
         linkLi.appendChild(link);
