2bb760bb21edb32f8cb64775d6b94a134cf251e7
OlikQueen <olysoz@mail.ru>
2024-04-07 20:10:26 +0300

    choice-city

--- src/js/choice-city.js
+++ src/js/choice-city.js
@@ -1,6 +1,8 @@
-export function choiceCity() {
-    // { name: "Москва", "subdomain": "msk"}
+/* 
+product.html#3
+*/
 
+export function choiceCity() {
     const citys = [
         {
             name: "Москва",
@@ -28,18 +30,53 @@ export function choiceCity() {
         },
     ];
 
-    const city = document.querySelector("[data-choice-city]");
-    const cityList = city.querySelector("[data-choice-list]");
+    const cityNode = document.querySelector("[data-choice-city]");
+    const cityListNode = cityNode.querySelector("[data-choice-list]");
 
-    for (const iterator of citys) {
-        const link = document.createElement('a');
-        link.innerHTML = iterator.name;
-        link.classList.add('choice-city__link');
-
-        const linkLi = document.createElement('li');
-        linkLi.classList.add('choice-city__item');
-
-        linkLi.appendChild(link);
-        cityList.appendChild(linkLi);
+    function createCitys(citys) {
+        cityListNode.innerHTML = "";
+        
+        for (const city of citys) {
+            const link = document.createElement('a');
+            link.innerHTML = city.name;
+            link.classList.add('choice-city__link');
+            link.setAttribute('href', 'subdomain');
+    
+            const linkLi = document.createElement('li');
+            linkLi.classList.add('choice-city__item');
+    
+            linkLi.appendChild(link);
+            cityListNode.appendChild(linkLi);
+        }
     }
+
+    createCitys(citys);
+
+    // 1. вывести value из input в консоль
+    // 2. написать регулярку
+    // 3. вывести города
+    // 4. синхронизировать поиск с базой
+
+    // 1. сделать метод
+    // 2. сформировать динамический массив
+
+    // ДЗ. uppercase === lowercase +
+
+    const inputNode = cityNode.querySelector("input");
+    inputNode.addEventListener('input', function () {
+        const newCitys = [];
+        const searchValue = inputNode.value.trim().toLowerCase();
+
+        for (const city of citys) {
+            // const regex = /а/g;
+            let regex = new RegExp(`^${searchValue}`,"i");
+
+            let found = city.name.match(regex);
+
+            if (Array.isArray(found) && found.length > 0) {
+                newCitys.push(city)
+            }
+        }
+        createCitys(newCitys);
+    })
 }
