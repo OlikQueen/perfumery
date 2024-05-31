2c776116ffc9fd25ee66ca7e0c690f53875d0a7d
OlikQueen <olysoz@mail.ru>
2024-05-31 20:10:01 +0300

    showcase-в_разработке

--- main.js
+++ main.js
@@ -6,11 +6,9 @@ setupCounter()
 import { modal } from './src/js/modal.js'
 modal()
 
-import { choiceCity } from './src/js/choice-city.js'
-choiceCity()
+// import { choiceCity } from './src/js/choice-city.js'
+// choiceCity()
 
 
 import { slider } from "slider00";
-// import './node_modules/slider00/dist/slider00.css';
-
-slider();
+import './node_modules/slider00/dist/slider00.css';
