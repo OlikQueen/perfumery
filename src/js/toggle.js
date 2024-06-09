29ab450e464e2ab119ed84cd6f35ed8ff8e789f4
OlikQueen <olysoz@mail.ru>
2024-06-09 22:25:04 +0300

    toggle

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,5 +1,14 @@
 export function toggle() {
 
+    // const toggle = document.querySelectorAll("[data-toggle]");
+    // for (const iterator of toggle) {
+    //     const button = iterator.querySelector("button");
+
+    //     button.addEventListener('click', function () {
+    //         iterator.classList.toggle('is-show');
+    //     })
+    // }
+
     const toggle = document.querySelectorAll("[data-toggle]");
     for (const iterator of toggle) {
         const button = iterator.querySelector("button");
